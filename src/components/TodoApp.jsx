23f4e5384fb65ff6303d422a0e0ae6ed1ce041b3
MdAasif06 <abdurm477@gmail.com>
2025-10-09 12:28:13 +0530

    edit func work proper

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,9 +3,15 @@ import React, { useState } from "react";
 const TodoApp = () => {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([
-    { task: "Make Dosa for mummy", completed: true },
+    { task: "Make Dosa for mummy-1", completed: true },
+    { task: "Make Dosa for mummy-2", completed: true },
+    { task: "Make Dosa for mummy-3", completed: true },
+    { task: "Make Dosa for mummy-4", completed: true },
     { task: "Make Dosa for fufi", completed: false },
   ]);
+
+  const [editIndex, setEditIndex] = useState("");
+  const [editTask, setEditTask] = useState("");
 
   const addTask = (e) => {
     // e.preventDefault()
@@ -22,11 +28,21 @@ const TodoApp = () => {
     setTasks([]);
   };
 
-  const clearCompletedTask=()=>{
-    const newTask=tasks.filter((ele,_)=>ele.completed =! true)
-    setTasks(newTask)
+  const clearCompletedTask = () => {
+    const newTask = tasks.filter((ele, _) => (ele.completed = !true));
+    setTasks(newTask);
+  };
 
-  }
+  const editingTask = (index) => {
+    if (editIndex === index) {
+      tasks[editIndex].task = editTask;
+      setEditTask("");
+      setEditIndex("");
+    } else {
+      setEditTask(tasks[index].task);
+      setEditIndex(index);
+    }
+  };
 
   return (
     <div className="h-screen bg-black text-white pt-15">
@@ -73,27 +89,40 @@ const TodoApp = () => {
           {tasks.map((ele, index) => (
             <div
               key={index}
-              className="flex justify-between bg-[#222] px-3 py-2 rounded-lg"
+              className="flex justify-between bg-[#222] px-3 py-2 rounded-lg gap-5"
             >
-              <div className="flex gap-2">
+              {editIndex === index ? (
                 <input
-                  className="accent-green-400"
-                  type="checkbox"
-                  id={index}
+                  autoFocus={true}
+                  value={editTask}
+                  onChange={(e) => setEditTask(e.target.value)}
+                  onKeyDown={(e) => e.key == "Enter" && editingTask(index)}
+                  className="outline-none border-b-2 border-green-400 w-full"
                 />
-                {/* only htmlFor work with label */}
-                <label
-                  htmlFor={index}
-                  className="text-lg cursor-pointer select-none"
-                >
-                  {ele.task}
-                </label>
-              </div>
+              ) : (
+                <div className="flex gap-2">
+                  <input
+                    className="accent-green-400"
+                    type="checkbox"
+                    id={index}
+                  />
+                  <label
+                    htmlFor={index}
+                    className="text-lg cursor-pointer select-none"
+                  >
+                    {ele.task}
+                  </label>
+                </div>
+              )}
               <div className="flex gap-2">
-                <span className="cursor-pointer h-5 w-5 bg-yellow-400 rounded-full p-2"></span>
                 <span
                   onClick={() => deleteTask(index)}
                   className="cursor-pointer h-5 w-5 bg-red-400 rounded-full p-2"
+                ></span>
+                <span
+                  onClick={() => editingTask(index)}
+                  className={`cursor-pointer h-5 w-5 rounded-full p-2
+                  ${editIndex === index ? "bg-green-400" : "bg-yellow-300"}`}
                 ></span>
               </div>
             </div>
@@ -102,7 +131,10 @@ const TodoApp = () => {
 
         {/* last portion of this project */}
         <div className="flex gap-4 font-medium">
-          <button onClick={clearCompletedTask} className="cursor-pointer px-3 py-2 rounded-lg bg-red-400 text-black">
+          <button
+            onClick={clearCompletedTask}
+            className="cursor-pointer px-3 py-2 rounded-lg bg-red-400 text-black"
+          >
             Clear Completed
           </button>
           <button
